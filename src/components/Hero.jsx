dd72894a936f0c1c2dc319dafcadb1881b4c62d5
Naman Bhardwaj <namanbhardwaj.official@gmail.com>
2024-11-14 01:21:51 +0530

    computer import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Naman</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
-            interfaces and web applications
+            I am a Front-End developer <br className="sm:block hidden" />
+            with expericence in Js and React!
           </p>
         </div>
       </div>
