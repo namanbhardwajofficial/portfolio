7307067d6f02e098db3330f514909cc96f7ebdc0
Naman Bhardwaj <namanbhardwaj.official@gmail.com>
2024-11-14 01:47:37 +0530

    loader import

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,6 +10,7 @@ const Computers = ({ isMobile }) => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,7 +19,6 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
